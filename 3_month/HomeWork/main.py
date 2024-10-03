c5b264beb91e9d7a705ee95aee7b72a2cd942a2e
EmirKerimberdiev <keremberdievemir43@gmail.com>
2024-10-04 02:53:23 +0600

    add HomeWork 1 from 3 month, and some other chenge

---
+++ 3_month/HomeWork/main.py
@@ -0,0 +1,44 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+from dotenv import dotenv_values
+import random
+import logging
+
+token = dotenv_values(".env")['token']
+bot = Bot(token=token)
+dp = Dispatcher()
+
+
+@dp.message(Command(commands=['start']))
+async def start_handler(message):
+    name = message.from_user.first_name
+    await message.answer(f"Добро {name} пожаловать на бот Backend_bot этот бот был создан для практики в качестве бота для меня!")
+
+@dp.message(Command(commands=['pic']))
+async def pic_handler(message):
+    image = types.FSInputFile('ea97ef2e-793a-4513-ad2c-82d7b340263c.webp')
+    await message.answer_photo(image)
+
+@dp.message(Command(commands=['myinfo']))
+async def myinfo_handler(message):
+    name = message.from_user.first_name
+    await message.answer(f'Ваше имя: {name}\nВаш id: {message.from_user.id}\nВаш username: {message.from_user.username}')
+
+@dp.message(Command(commands=['random']))
+async def random_handler(message):
+    await message.answer(random.choice(["Данил", "Акель", "Дастан", "Адина", "Лиля", "Айсуллу"]))
+
+@dp.message()
+async def echo_handler(message):
+    text = message.text
+    await message.answer(text)
+
+
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Hello this my project!
 
-8 HomeWork in 2_month\Home_Work\home_work_2_8
-I add file home_work_2_7 and this file create countries.db in this file has all tables.
+1 HomeWork in 3_month\Home_Work
+I add folder 3_month,Home_Work,Lesson and in folder Home_Work I do my homework.
